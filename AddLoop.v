84c747933286da72e11e3532974955a391e60ed3
Matt Quinn <matt@mattjquinn.com>
2019-02-10 11:02:32 -0500

    Adding decisional equality for registers.

--- AddLoop.v
+++ AddLoop.v
@@ -1,8 +1,5 @@
 (* TODO: Replace Z with 32bit datatype
    everywhere applicable.
-   TODO: Switch from using strings as register
-   names to using an inductive type; will require
-   defnining decisional equality for the type.
 *)
 
 Require Import List String Omega ZArith.
@@ -13,20 +10,23 @@ Local Open Scope Z_scope.
 
 Import ListNotations.
 
+Inductive reg : Set := r3 | r6 | r8.
+Definition reg_dec : forall r r' : reg, {r = r'} + {r <> r'}.
+Proof. intros. decide equality. Defined.
+
 
 Definition eqb_string (x y : string) : bool :=
   if string_dec x y then true else false.
-Definition total_map (A : Type) := string -> A.
+Definition total_map (A : Type) := reg -> A.
  Definition t_empty {A : Type} (v : A) : total_map A := (fun _ => v).
-Definition t_update {A : Type} (m : total_map A) (x : string) (v : A) :=
-  fun x' => if (eqb_string x x') then v else m x'.
+Definition t_update {A : Type} (m : total_map A) (x : reg) (v : A) :=
+  fun x' => if (reg_dec x x') then v else m x'.
 
 Definition reg_state := total_map Z.
-(* Inductive reg : Set := | r6. *)
 
 Inductive ins : Type :=
 | seq : ins -> ins -> ins
-| mov : string -> Z -> ins.
+| mov : reg -> Z -> ins.
 
 Notation "i1 ;; i2" :=
   (seq i1 i2) (at level 80, right associativity).
@@ -42,13 +42,13 @@ Inductive armR : ins -> reg_state -> reg_state -> Prop :=
 (* ====================================================================== *)
 
 Definition AddLoopArm : ins :=
-  (mov "r6" 5) ;;
-  (mov "r3" 2) ;;
-  (mov "r6" 8).
+  (mov r6 5) ;;
+  (mov r3 2) ;;
+  (mov r6 8).
   
 Lemma finalstate8: forall st st' : reg_state,
          (armR AddLoopArm st st') ->
-         st' "r6" = 8.
+         st' r6 = 8.
 Proof.
   unfold AddLoopArm. intros. inversion H. subst.
   inversion H5. subst. inversion H7. subst. unfold t_update.
